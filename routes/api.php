71ddfe50b1443083bfeba7f6aa7464bed3453985
Jean Lionel <nijeanlionel@gmail.com>
2025-09-11 07:30:52 +0200

    init api liste users

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,13 @@
 <?php
 
+namespace App\Http\Controllers;
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+
+Route::post('/webhook', [WebhookController::class, 'handle']);
