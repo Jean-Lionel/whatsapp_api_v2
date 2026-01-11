3ecee773601b6837ffdf30912e9b0be005125722
Jean-Lionel <nijeanlionel@gmailcom>
2026-01-11 12:56:07 +0200

    add authentification

--- routes/api.php
+++ routes/api.php
@@ -5,13 +5,20 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WhatsappDataController;
+use App\Http\Controllers\AuthController;
 
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
 
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    });
+
+    Route::post('/logout', [AuthController::class, 'logout']);
+
+    Route::apiResource('whatsapp-data', WhatsappDataController::class);
+});
 
 Route::post('/webhook', [WebhookController::class, 'handle']);
-
-Route::apiResource('whatsapp-data', WhatsappDataController::class);
